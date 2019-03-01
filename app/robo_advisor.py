56afc27d853aa0229c331b88265d297abd8f598c
mgb80 <46605250+mgb80@users.noreply.github.com>
2019-02-28 22:15:28 -0500

    created dates[]

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -34,8 +34,15 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close = parsed_response["Time Series (Daily)"]["2019-02-28"]["4. close"]
-#breakpoint()
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys()) #sort to ensure that the latest day is first
+
+latest_day = dates[0]
+
+latest_close = tsd[latest_day]["4. close"]
+
+
 
 # TODO: further parse the JSON response...
 
