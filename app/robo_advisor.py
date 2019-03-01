7dd636a4420472824d95a3bb2e299fa48ada57e5
mgb80 <46605250+mgb80@users.noreply.github.com>
2019-03-01 17:10:26 -0500

    Reccomendation added

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,7 @@ import os
 import requests
 import datetime
 import csv
+import statistics 
 
 
 
@@ -41,20 +42,6 @@ while x == 1:
 
 
 
-
-
-
-#symbol = input("Please enter the stock symbol you'd like to analyze: ") # TODO: capture user input, like... input("Please specify a stock symbol: ")
-
-# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
-# TODO: assemble the request url to get daily data for the given stock symbol...
-
-
-
-
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
-
-
 #print(type(response))
 #print(response.status_code)
 #print(response.text)
@@ -70,17 +57,22 @@ latest_close = tsd[latest_day]["4. close"]
 
 high_price_list = []
 low_price_list = []
+close_price_list = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
     high_price_list.append(float(high_price))
     low_price = tsd[date]["3. low"]
     low_price_list.append(float(low_price))
+    close_p = tsd[date]["4. close"]
+    close_price_list.append(float(close_p))
 
 
 #max of all the high prices...
 recent_high = max(high_price_list) 
 
+close_av = 120000
+#close_av = statistics.mean(close_price_list)
 
 recent_low = min(low_price_list)
 
@@ -136,17 +128,24 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
 t = datetime.datetime.now()
 t.strftime("%Y-%m-%d %I:%M %p")
 
-print("-----------------")
+print("-----------------------------------")
 print(f"STOCK SYMBOL: {sym_input}")
 print("RUN AT: " + t.strftime("%Y-%m-%d %I:%M %p"))
-print("-----------------")
+print("-----------------------------------")
 print(f"LATEST DAY OF AVAILABLE DATA: {last_refreshed}" )
 print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH CLOSING PRICE: {to_usd(float(recent_high))}")
 print(f"RECENT LOW CLOSING PRICE:{to_usd(float(recent_low))}")
-print("-----------------")
-print("RECOMMENDATION: Buy!")
-print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
-print("-----------------")
+
+
+# GIVE RECOMMENDATION__________________________
+print("-----------------------------------")
+if (  float(latest_close) > float(close_av)  ):
+    print("RECOMMENDATION: Buy!")
+    print("REASONING: The latest closing price is higher than the average closing price.")
+else:
+    print("RECOMMENDATION: DON'T BUY!")
+    print("REASON: Because the latest closing price is less than the average closing price, we recommend you do not invest.") #REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+print("-----------------------------------")
 print("Writing info to csv" + str(csv_file_path))
 
